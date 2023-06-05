76abcb1288e80244a114e269ddb7f23aeb1724b7
ajay reddy <ajayreddy7474@gmail.com>
2023-06-05 21:27:35 +0530

    brochill site

---
+++ left.js
@@ -0,0 +1,35 @@
+ScrollReveal().reveal(' .one .matter1-container',
+    { origin: 'left' ,
+     easing: 'ease' ,
+     delay: 100 ,
+    distance: '600px',
+    duration: 2000   }
+
+);
+
+ScrollReveal().reveal('.image',
+    { origin: 'right' ,
+     easing: 'ease' ,
+     delay: 100 ,
+    distance: '600px',
+    duration: 2000   }
+
+);
+
+ScrollReveal().reveal(' .second-matter1-container',
+    { origin: 'left' ,
+     easing: 'ease' ,
+     delay: 100 ,
+    distance: '600px',
+    duration: 2000   }
+
+);
+
+ScrollReveal().reveal(' .third-matter1-container',
+    { origin: 'left' ,
+     easing: 'ease' ,
+     delay: 100 ,
+    distance: '600px',
+    duration: 2000   }
+
+);
